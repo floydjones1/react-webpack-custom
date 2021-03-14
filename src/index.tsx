f5b193c968d03450ca933722ff07cd00bcdcc0b4
Floyd Jones <floyd-jones1@hotmail.com>
2021-03-13 20:46:58 -0500

    fixes html plugin path

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from '@material-ui/core/Button';
-import Navbar from './components/Navbar'
 
 // import default style
-import './main.css'
+import "./main.css";
 
 function App() {
-  let num: number = 1
-  num = 2
+  let num: number = 1;
+  num = 2;
   return (
     <div>
-      <Navbar label='login' />
       <h1>Hello blog readers {num}</h1>
-      <Button variant="contained" color="primary">Hi</Button>
     </div>
   );
 }
-
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       favicon: false,
-      template: './src/public/index.html'
+      template: './public/index.html'
     }),
     new ForkTsCheckerWebpackPlugin()
   ],
